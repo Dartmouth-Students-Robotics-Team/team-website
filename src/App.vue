2240cdc1c4ad857c43f3b3724001f85a514ac019
Arthur Monteath Trindade <arthur.monteath@gmail.com>
2025-10-15 21:47:44 -0400

    Added logo

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 
 <template>
   <h1>You did it!</h1>
+  
+  <img src="DSRT_Logo.webp" alt="DSRT Logo">
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
