ccb18366417ab29083f4c464bfa5fd10bd698b9c
Arthur Monteath Trindade <arthur.monteath@gmail.com>
2025-10-19 16:22:12 -0400

    Removed font fully and removed dev tools from prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwind from '@tailwindcss/postcss'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/team-website/',
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    ...(mode === 'development' ? [vueDevTools()] : []),
+  ],
   css: { postcss: { plugins: [tailwind()] } },
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
-})
+}))
