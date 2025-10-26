74b507014899f86c8ca00cb48a64b633f44e1700
Arthur Monteath Trindade <arthur.monteath@gmail.com>
2025-10-26 00:30:17 -0400

    Fixed order of the navbar

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -7,9 +7,9 @@
       </a>
       <ul class="gap-5 text-ink/85 text-sm hidden md:flex">
         <li><a href="#robots" class="hover:text-accent transition-colors">Robots</a></li>
-        <li><a href="#results" class="hover:text-accent transition-colors">Results</a></li>
-        <li><a href="#team" class="hover:text-accent transition-colors">Team</a></li>
+        <!--<li><a href="#results" class="hover:text-accent transition-colors">Results</a></li>-->
         <li><a href="#sponsors" class="hover:text-accent transition-colors">Sponsors</a></li>
+        <li><a href="#team" class="hover:text-accent transition-colors">Team</a></li>
         <li>
           <a href="#join" class="px-3 py-2 rounded-xl font-bold text-bg bg-gradient-to-b from-accent to-accent-dark">
             Join Us
